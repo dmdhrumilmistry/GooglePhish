645b37fd76117428d57a3acfce4e67ec18511933
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2021-09-13 01:41:05 +0530

    change settings for production server

--- GooglePhish/settings.py
+++ GooglePhish/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q0b_ojdw#k*7^s7*+#dzgb*kb=f%zr+xd82j9=us9vxh8k_n2!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
